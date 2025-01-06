710300805a6d098632a879e85c1112e3ec40f8da
Luis Fernando Vallejos Copa <155495125+Lferd10@users.noreply.github.com>
2025-01-06 19:58:49 +0000

    update

---
+++ clase3/menuv.php
@@ -0,0 +1,9 @@
+<div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+    Menu
+  </a>
+  <a href="#" class="list-group-item list-group-item-action">Inicio</a>
+  <a href="#" class="list-group-item list-group-item-action">Productos</a>
+  <a href="#" class="list-group-item list-group-item-action">Clientes</a>
+  <a class="list-group-item list-group-item-action disabled" aria-disabled="true">Cerrar sesion</a>
+</div>
