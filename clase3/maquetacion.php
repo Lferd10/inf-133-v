c68d4a53b2414a9add40e0ad77ce2479439137fb
Luis Fernando Vallejos Copa <155495125+Lferd10@users.noreply.github.com>
2025-01-06 19:17:15 +0000

    update

---
+++ clase3/maquetacion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row border">Cabezera</div>
+        <div class="row border">Menu Horizontal</div>
+        <div class="row border">
+            <div class="col-12 col-md-3 col-lg-4 border">MENU</div>
+            <div class="col-12 col-md-9 col-lg-8 border">PRINCIPAL</div>
+        </div>
+        <div class="row border">Pie de pagina</div>
+    </div>
+
+
+</body>
+</html>
